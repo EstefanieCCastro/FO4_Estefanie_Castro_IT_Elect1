129eb8922eb99eacf81aef9a840bdcb4f6bb34e3
deasyewan <deasyriesanico26@gmail.com>
2025-09-20 18:05:16 +0800

    Midterm

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-
-/*/ App.js
-import React from 'react';
+/*import React from 'react';
 import { SafeAreaView } from 'react-native';
 import CounterApp from './CounterApp';
 import ColorChangerApp from './ColorChangerApp';
